93426ec11e2e8a92d76ae26790f03152eeb85127
Frederico Ozanam <frederico.o.souza@gmail.com>
2021-10-06 10:08:39 -0300

    Assumindo o controle do JDBC

--- loja-virtual-repository/src/TestaInsercaoComParametro.java
+++ loja-virtual-repository/src/TestaInsercaoComParametro.java
@@ -7,27 +7,37 @@ import java.sql.Statement;
 public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
-		String nome = "Mouse'''''''";
-		String descricao = "Mouse sem fio'''''''";
-		
+
 		ConnectionFactory factory = new ConnectionFactory();
 		Connection connection = factory.RecuperarConexao();
-		
-		//PreparedStatemt que cuida da inserção. SetString passa os parametros, index e parametro
-		PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
-		
+		connection.setAutoCommit(false);
+
+		// PreparedStatemt que cuida da inserção. SetString passa os parametros, index e
+		// parametro
+		PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (?, ?)",
+				Statement.RETURN_GENERATED_KEYS);
+
+		adicionarVariavel("SmartTV", "50 polegadas", stm);
+		adicionarVariavel("Home Theater", "Onkyo 5.1 canais", stm);
+
+	}
+
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
-		
+
+		/*
+		 * if (nome.equals("Home Theater")) { throw new
+		 * RuntimeException("Não foi possível adicionar o produto"); }
+		 */
 		stm.execute();
-		
 
 		ResultSet rst = stm.getGeneratedKeys();
 		while (rst.next()) {
 			Integer id = rst.getInt(1);
 			System.out.println("O id criado foi: " + id);
 		}
-
+		rst.close();
 	}
 
 }
