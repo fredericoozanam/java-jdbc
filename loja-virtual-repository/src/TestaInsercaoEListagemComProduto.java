0effc3fe6d5a588ec9fca3c4636ca799c50311b7
Frederico Ozanam <frederico.o.souza@gmail.com>
2021-10-11 11:47:14 -0300

    Listando no ProdutoDAO

--- loja-virtual-repository/src/TestaInsercaoEListagemComProduto.java
+++ loja-virtual-repository/src/TestaInsercaoEListagemComProduto.java
@@ -3,22 +3,25 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 import br.com.alura.jdbc.ConnectionFactory;
 import dao.ProdutoDAO;
 import modelo.Produto;
 
-public class TestaInsercaoComProduto {
+public class TestaInsercaoEListagemComProduto {
 
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
 
-		Produto comoda = new Produto("Hack", "Hack horizontal");
+		Produto comoda = new Produto("Cômoda", "Cômoda vertical");
 
 		try (Connection connection = new ConnectionFactory().RecuperarConexao()) {
 			ProdutoDAO produtoDao = new ProdutoDAO(connection);
 			produtoDao.salvar(comoda);
-			// Lista = persistenciaProduto.listar();
+
+			List<Produto> listaDeProdutos = produtoDao.listar();
+			listaDeProdutos.stream().forEach(lp -> System.out.println(lp));
 		}
 	}
 
--- loja-virtual-repository/src/br/com/alura/jdbc/TestaRemocao.java
+++ loja-virtual-repository/src/br/com/alura/jdbc/TestaRemocao.java
@@ -1,4 +1,5 @@
 package br.com.alura.jdbc;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
